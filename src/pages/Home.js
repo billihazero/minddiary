e05897cd22d99fc97eeccc35c783efe7cfb65ae5
billihazero <billihazero@gmail.com>
2024-11-12 09:37:54 +0900

    feat: button 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
 import Button from "../component/Button";
+
 const Home = () => {
   return (
     <div>
       <Button
-        text={"버튼 텍스트"}
+        text={"기본버튼"}
         onClick={() => {
-          alert("hi");
+          alert("default button");
+        }}
+      />
+      <Button
+        text={"긍정버튼"}
+        type={"positive"}
+        onClick={() => {
+          alert("positive button");
+        }}
+      />
+      <Button
+        text={"부정버튼"}
+        type={"negative"}
+        onClick={() => {
+          alert("negative button");
         }}
       />
     </div>
